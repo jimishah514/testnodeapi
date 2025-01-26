17b731adb53057efdf6d8a898f708099d50e953e
jamalmoosvi <jamalmoosvi@gmail.com>
2025-01-26 15:07:20 +0500

    added default route

--- index.mjs
+++ index.mjs
@@ -3,6 +3,11 @@ import express from "express";
 const port = 3000;
 const app = express();
 
+// Define a route for the root URL
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, World!</h1>");
+});
+
 app.get("/sayHello", async (req, res) => {
   return res.json({ hello: "hello" });
 });
